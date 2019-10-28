af1442828ea9ecef3c5cad743ed81d1b27e27f51
Matt Gahrns <matthewgahrns@hotmail.com>
2019-10-28 11:38:29 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Matt"
+    end
+
+    get '/hometown' do 
+        "My hometown is Woodinville"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is We'll Be Coming Back"
+    end
+
+
+
+
 end
